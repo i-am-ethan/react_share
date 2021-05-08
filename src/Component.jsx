06fbbde73a79433f680267e2205556b2600ffb03
cawauchi6204 <cawauchi6204@gmail.com>
2021-05-08 11:52:19 +0900

    fuga

--- src/Component.jsx
+++ src/Component.jsx
@@ -7,9 +7,3 @@ export const About = () => <p>about</p>
 
 export const Store = () => <p>store</p>
 export const Guitar = () => <p>guitar</p>
-
-// <Route path="/top" children={<Top />} />
-// <Route path="/news" children={<News />} />
-// <Route path="/about" children={<About />} />
-// <Route path="/store" children={<Store />} />
-// <Route path="/guitar" children={<Guitar />} />
